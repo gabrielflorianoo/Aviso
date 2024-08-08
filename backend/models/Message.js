9da1b6ec1bdbbd0fc66186e43f8dd8b53f20b06b
gabrielflorianoo <tacacheee221@gmail.com>
2024-08-08 14:15:13 -0300

    Fixed users route

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -5,12 +5,14 @@ const UserSchema = Joi.object({
 	messageID: Joi.string(),
 	message: Joi.string().min(1).max(300).required(),
 	userID: Joi.string().min(3).max(30).required(),
+	creatDate: Joi.date()
 });
 
 function ValidateMessage(req, res, next) {
 	// The Id of the user must be its username
 	req.body.messageID = uuid.v4();
 	req.body.userID = req.session.user.username;
+	req.body.createDate = new Date.now();
 	console.log(req.body);
 
 	const { error } = UserSchema.validate(req.body);
