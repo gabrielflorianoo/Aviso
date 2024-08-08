7aa89de491b6f4bd1579d2b5c4fa70fbadf27f6b
gabrielflorianoo <tacacheee221@gmail.com>
2024-08-08 11:00:19 -0300

    Created repository

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+	<Navbar />
+	<section class="is-full-desktop">
+		<div class="columns p-2">
+			<div class="column is-2" style="background-color: aqua;">Left Column</div>
+			<div class="column" style="background-color: bisque;">Middle column</div>
+			<div class="column is-2" style="background-color: chartreuse;">Right column</div>
+		</div>
+	</section>
+</template>
+
+<style>
+  section {
+    height: 100vh;
+  }
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+	<nav class="navbar" role="navigation" aria-label="main navigation">
+		<div class="navbar-brand">
+			<a class="navbar-item" href="">
+				<img class="img" src="../assets/Logo-Transparent.PNG" alt="Logo" />
+			</a>
+		</div>
+
+		<div id="navbarBasicExample" class="navbar-menu">
+			<div class="navbar-end">
+				<div class="navbar-item">
+					<div class="buttons">
+						<a class="button is-primary">
+							<strong>Sign up</strong>
+						</a>
+						<a class="button is-light"> Log in </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<style>
+    .img {
+        min-height: 50px;
+    }
+</style>
