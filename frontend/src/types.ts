26f58a4821af5fdecc2e7976775877c39ea855a9
gabrielflorianoo <tacacheee221@gmail.com>
2024-08-09 14:29:49 -0300

    Formated all items again (have to cancel before because of node_modules)

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,13 +1,13 @@
 interface Message {
-    messageID: string;
-    message: string;
-    userID: string;
-    creatDate?: Date; // Opcional, se a data de criação for opcional
+  messageID: string;
+  message: string;
+  userID: string;
+  creatDate?: Date; // Opcional, se a data de criação for opcional
 }
 
 interface User {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export type { Message, User };
