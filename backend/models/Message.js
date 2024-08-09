0adaf3511893a57b6b6356d77c1dedc0f204a642
gabrielflorianoo <tacacheee221@gmail.com>
2024-08-09 14:29:49 -0300

    Formated all items again (had to cancel before because of node_modules)

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,28 +1,28 @@
-const { json } = require('express');
-const Joi = require('joi');
-const uuid = require('uuid');
+const { json } = require("express");
+const Joi = require("joi");
+const uuid = require("uuid");
 
 const UserSchema = Joi.object({
-    messageID: Joi.string().required(),
-    message: Joi.string().min(1).max(300).required(),
-    userID: Joi.string().min(3).max(30).required(),
-    createDate: Joi.date().required(),
+  messageID: Joi.string().required(),
+  message: Joi.string().min(1).max(300).required(),
+  userID: Joi.string().min(3).max(30).required(),
+  createDate: Joi.date().required(),
 });
 
 function ValidateMessage(req, res, next) {
-    // The Id of the user must be its username
-    req.body.messageID = uuid.v4();
-    req.body.userID = req.session.user.username;
-    req.body.createDate = new Date();
-    console.log(req.body);
+  // The Id of the user must be its username
+  req.body.messageID = uuid.v4();
+  req.body.userID = req.session.user.username;
+  req.body.createDate = new Date();
+  console.log(req.body);
 
-    const { error } = UserSchema.validate(req.body);
+  const { error } = UserSchema.validate(req.body);
 
-    if (error) {
-        return res.status(400).send(error.details[0].message); // Use 400 para erros de validação
-    }
+  if (error) {
+    return res.status(400).send(error.details[0].message); // Use 400 para erros de validação
+  }
 
-    next();
+  next();
 }
 
 module.exports = ValidateMessage;
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,30 +1,26 @@
-import vue from '@vitejs/plugin-vue';
-import { join, parse, resolve } from 'path';
+import vue from "@vitejs/plugin-vue";
+import { join, parse, resolve } from "path";
 
 export default {
-    plugins: [vue()],
-    alias: {
-        '~': __dirname,
+  plugins: [vue()],
+  alias: {
+    "~": __dirname,
+  },
+  build: {
+    rollupOptions: {
+      input: entryPoints("index.html", "foo/index.html", "foo/bar/index.html"),
     },
-    build: {
-        rollupOptions: {
-            input: entryPoints(
-                'index.html',
-                'foo/index.html',
-                'foo/bar/index.html'
-            ),
-        },
-    },
+  },
 };
 
 function entryPoints(...paths) {
-    const entries = paths.map(parse).map((entry) => {
-        const { dir, base, name, ext } = entry;
-        const key = join(dir, name);
-        const path = resolve(__dirname, dir, base);
-        return [key, path];
-    });
+  const entries = paths.map(parse).map((entry) => {
+    const { dir, base, name, ext } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
 
-    const config = Object.fromEntries(entries);
-    return config;
+  const config = Object.fromEntries(entries);
+  return config;
 }
