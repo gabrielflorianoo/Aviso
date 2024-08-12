00a273b7fa48b9aef4779378ee1130751e48947c
gabrielflorianoo <tacacheee221@gmail.com>
2024-08-12 08:40:32 -0300

    Commented index.ts

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -11,6 +11,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createRouter, createWebHistory } from 'vue-router';
 
+// Creates the routes for the router element
 const routes = [
     { path: '/', component: Home },
     { path: '/signup', component: SignUp },
@@ -18,12 +19,19 @@ const routes = [
     { path: '/createMessage', component: CreateMessage },
 ];
 
+// Creates the router
 const router = createRouter({
+    /* 
+        'history' - Uses browser's history.pushState
+        to change the URL without realoding the page
+    */
     history: createWebHistory(),
     routes,
 });
 
 const pinia = createPinia();
+
+// Ensures that the global state is preserved across page reloads and navigations
 pinia.use(persistedState);
 
 createApp(App).use(pinia).use(router).mount('#app');
